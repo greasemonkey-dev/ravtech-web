81b13b7b5764725e3ae2aa132c791ce4cfe81be0
admin <admin@ravtech.co.il>
2025-10-30 10:30:03 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,6 +1,7 @@
 import Home from './pages/Home';
 import AccelerateStartups from './pages/AccelerateStartups';
 import Services from './pages/Services';
+import Solutions from './pages/Solutions';
 import Layout from './Layout.jsx';
 
 
@@ -8,6 +9,7 @@ export const PAGES = {
     "Home": Home,
     "AccelerateStartups": AccelerateStartups,
     "Services": Services,
+    "Solutions": Solutions,
 }
 
 export const pagesConfig = {
