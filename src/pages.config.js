cb1e02fd9fa19c710c4030006d1d4819c61f2806
admin <admin@ravtech.co.il>
2025-11-02 14:32:42 +0000

    Our Team Section Added

--- src/pages.config.js
+++ src/pages.config.js
@@ -7,7 +7,6 @@ import CaseStudies from './pages/CaseStudies';
 import Careers from './pages/Careers';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import TeamMemberDetail from './pages/TeamMemberDetail';
 import Layout from './Layout.jsx';
 
 
@@ -21,7 +20,6 @@ export const PAGES = {
     "Careers": Careers,
     "About": About,
     "Contact": Contact,
-    "TeamMemberDetail": TeamMemberDetail,
 }
 
 export const pagesConfig = {
