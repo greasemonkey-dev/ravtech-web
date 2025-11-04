8e6e35e4d2f9f197755e495f00c9f88535b5b9b8
admin <admin@ravtech.co.il>
2025-11-04 12:52:37 +0000

    Manual code change

--- src/components/LinkedInIcon.jsx
+++ src/components/LinkedInIcon.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const LinkedInIcon = ({ className = "w-5 h-5" }) => {
+export const LinkedInIcon = ({ className = "w-8 h-8" }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
