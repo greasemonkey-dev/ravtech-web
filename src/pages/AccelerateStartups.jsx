cf7224aca18ebda41408b878dbacb64bcae01190
admin <admin@ravtech.co.il>
2025-11-10 08:30:38 +0000

    File changes

--- src/pages/AccelerateStartups.jsx
+++ src/pages/AccelerateStartups.jsx
@@ -233,7 +233,7 @@ export default function AccelerateStartupsPage() {
       <section className="py-24 px-4 sm:px-6 lg:px-8 bg-gray-50">
         <div className="max-w-7xl mx-auto">
           <h2 className="text-4xl sm:text-5xl font-bold text-center text-black mb-4">
-            What do you get in the acceleration program?
+            Accelerate your success- here's what you'll get
           </h2>
           <div className="bg-[#0373BA] mb-16 mx-auto w-24 h-1" />
 
